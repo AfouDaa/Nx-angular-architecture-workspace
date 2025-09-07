3aa7d778db34d7d8b51db4b5a400299b7c88e970
ahmed khaled <155364320+AfouDaa@users.noreply.github.com>
2025-09-07 15:42:55 +0300

    add data-access-layer to app module

--- org/apps/fdd-app/src/app/app.config.ts
+++ org/apps/fdd-app/src/app/app.config.ts
@@ -6,12 +6,14 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideEnvironment } from '@org/environment';
+import { provideDataAccess } from '@org/data-access-layer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideDataAccess(),
     provideEnvironment({
       contexts: { api: { name: 'api/v1', port: 8000 } },
       host: 'http://localhost',
