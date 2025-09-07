33d67ea6fc7a65e34c93caf622521186746fac77
ahmed khaled <155364320+AfouDaa@users.noreply.github.com>
2025-09-07 15:42:57 +0300

    enhance

--- org/libs/data-access-layer/src/lib/data-access-layer/data-access-layer.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-data-access-layer',
-  imports: [],
-  templateUrl: './data-access-layer.html',
-  styleUrl: './data-access-layer.css',
-})
-export class DataAccessLayer {}
