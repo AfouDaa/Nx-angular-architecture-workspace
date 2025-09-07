3f5325dfcdd87485552b4b0c7d47e53349fefa28
ahmed khaled <155364320+AfouDaa@users.noreply.github.com>
2025-09-07 15:28:06 +0300

    add env to app module

--- org/apps/fdd-app/src/app/app.config.ts
+++ org/apps/fdd-app/src/app/app.config.ts
@@ -5,11 +5,17 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideEnvironment } from '@org/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideEnvironment({
+      contexts: { api: { name: 'api/v1', port: 8000 } },
+      host: 'http://localhost',
+      production: false,
+    }),
   ],
 };
